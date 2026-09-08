e5d459c8b7b63018eb7fd36fc585f0da5d1c3170
ross58481 <ross58481@gmail.com>
2026-09-08 15:08:15 -0700

    updated the input message to ask for the user's favorite animal

--- projects/greeting-project/hello.py
+++ projects/greeting-project/hello.py
@@ -1,6 +1,6 @@
 # My first Python project in GitHub
 def greet_user():
-name = input("Enter your name: ")
+name = input("Enter your name and your favorite animal: ")
 print(f"Hello, {name}! Welcome to Data Science. My name is Luke and editing this text is my first major change to the code template")
 if __name__ == "__main__":
 greet_user()
